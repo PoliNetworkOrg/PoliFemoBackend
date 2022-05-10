bf45efac03320361e1b01c5ff388361b316b4a14
user <user@example.com>
2022-05-10 17:54:34 +0200

    Update HelloController.cs

---
+++ Source/Controllers/HelloController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PoliFemoBackend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HelloController : ControllerBase
+{
+    [HttpGet(Name = "GetHello")]
+    public IEnumerable<string> Get()
+    {
+        return new List<string> { "Hello!" };
+    }
+}
