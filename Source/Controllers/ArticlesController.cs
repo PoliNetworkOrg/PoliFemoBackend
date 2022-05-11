e9810bfe5094fafa7cfb624cdf0d6d0bf2e3cd60
Stig Narnia <stig.narnia@gmail.com>
2022-05-11 22:55:44 +0200

    first basic implementation of articles

--- Source/Controllers/ArticlesController.cs
+++ Source/Controllers/ArticlesController.cs
@@ -7,10 +7,47 @@ namespace PoliFemoBackend.Controllers;
 [Route("[controller]")]
 public class ArticlesController : ControllerBase
 {
-    [HttpGet]
-    public IActionResult Get(JObject payload)
+    [HttpPost]
+    public IActionResult SearchArticles(JObject payload)
     {
-        
-        return Ok(payload);
+        HttpClient client = new();
+        using HttpResponseMessage response = client.GetAsync("https://pastebin.com/raw/Giry1b7z").Result;
+        using HttpContent content = response.Content;
+        string data = content.ReadAsStringAsync().Result;
+        try
+        {
+            JObject articles = JObject.Parse(data);
+            List<JToken> results = Filter(payload, articles);
+            return Ok(results);
+        }
+        catch
+        {
+            return Ok("Couldn't reach Github to download articles");
+        }
+    }
+
+    private static List<JToken> Filter(JObject payload, JObject articles)
+    {
+        bool isEqual;
+        List<JToken> results = new();
+
+        foreach (JToken child in articles["articles"])
+        {
+            isEqual = true;
+            foreach (JProperty prop in payload.Properties())
+            {
+                if (prop.Value.ToString() != child[prop.Name].ToString())
+                {
+                    isEqual = false;
+                    break;
+                }
+            }
+            if (isEqual)
+            {
+                results.Add(child);
+            }
+        }
+
+        return results;
     }
 }
