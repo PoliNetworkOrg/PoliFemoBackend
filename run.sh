863cca7e1cc238611ae119134e0b1b4a2aeb883b
Stig Narnia <stig.narnia@gmail.com>
2022-05-12 14:09:44 +0200

    Things for server deployment

--- run.sh
+++ run.sh
@@ -1,32 +1,10 @@
-try
-{
-    var builder = WebApplication.CreateBuilder(args);
-
-    // Add services to the container.
-
-    builder.Services.AddControllers().AddNewtonsoftJson();
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-
-    var app = builder.Build();
-
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
-
-    app.UseHttpsRedirection();
-
-    app.UseAuthorization();
-
-    app.MapControllers();
-
-    app.Run();
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex);
-}
+#!/bin/sh
+while [ 1 != 0 ]
+do
+        pkill PoliFemoBackend
+        sleep 5
+        wget https://github.com/PoliNetworkOrg/PoliFemoBackend/releases/latest/download/PoliFemoBackend.run -O /home/ubuntu/PoliFemoBackend/PoliFemoBackend
+        chmod +x PoliFemoBackend
+        screen ./PoliFemoBackend
+        sleep 1d
+done
