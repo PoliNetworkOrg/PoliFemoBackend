8c9bba20ac9d0685bd10def00a00be3aa001de55
Stig Narnia <stig.narnia@gmail.com>
2022-05-14 11:33:13 +0200

    .

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ while [ 1 != 0 ]
 do
         pkill PoliFemoBackend
         sleep 5
-        wget https://github.com/PoliNetworkOrg/PoliFemoBackend/releases/latest/download/PoliFemoBackend.run -O /home/ubuntu/PoliFemoBackend/PoliFemoBackend
+        wget https://github.com/PoliNetworkOrg/PoliFemoBackend/releases/latest/download/PoliFemoBackend -O /home/ubuntu/PoliFemoBackend/PoliFemoBackend
         chmod +x PoliFemoBackend
         screen ./PoliFemoBackend
         sleep 1d
