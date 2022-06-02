9b7a577fb82ab4f5ce2141bb1286c092f9d2504d
Stig Narnia <stig.narnia@gmail.com>
2022-06-02 11:37:17 +0200

    .

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sleep 2
+sudo pkill PoliFemoBackend
 sudo iptables -P INPUT ACCEPT
 sudo iptables -P FORWARD ACCEPT
 sudo iptables -P OUTPUT ACCEPT
@@ -7,7 +8,7 @@ sudo iptables -t nat -F
 sudo iptables -t mangle -F
 sudo iptables -F
 sudo iptables -X
-wget https://github.com/PoliNetworkOrg/PoliFemoBackend/releases/latest/download/PoliFemoBackend.zip -O /home/ubuntu/PoliFemoBackend/PoliFemoBackend.zip
+wget https://github.com/PoliNetworkOrg/PoliFemoBackend/releases/latest/download/PoliFemoBackend.zip -q -O /home/ubuntu/PoliFemoBackend/PoliFemoBackend.zip
 unzip -o PoliFemoBackend.zip
 sudo chmod +x run.sh
 cp appsettings_production.json appsettings.json
