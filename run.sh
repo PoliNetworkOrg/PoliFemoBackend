76839298e48688cf8d30a6c5498478a2c027a889
Francesco Lo Faro <francesco@lofa.ml>
2022-08-23 22:13:48 +0000

    autodeploy with 2 screens

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sleep 2
-sudo pkill PoliFemoBackend
+screen -S backend -p 0 -X stuff "^C"
 sudo iptables -P INPUT ACCEPT
 sudo iptables -P FORWARD ACCEPT
 sudo iptables -P OUTPUT ACCEPT
@@ -8,11 +8,11 @@ sudo iptables -t nat -F
 sudo iptables -t mangle -F
 sudo iptables -F
 sudo iptables -X
-wget https://github.com/PoliNetworkOrg/PoliFemoBackend/releases/latest/download/PoliFemoBackend.zip -q -O /home/ubuntu/PoliFemoBackend/PoliFemoBackend.zip
+wget https://github.com/PoliNetworkOrg/PoliFemoBackend/releases/latest/downloads/PoliFemoBackend.zip -q -O PoliFemoBackend.zip
 unzip -o PoliFemoBackend.zip
 sudo chmod +x run.sh
 cp appsettings_production.json appsettings.json
 SSLCert=$(cat secrets.json | jq -r '.SSLCert')
 cat appsettings.json | jq --arg SSLCert "$SSLCert" '.Kestrel.Endpoints.HttpsInlineCertAndKeyFile.Certificate.Password |= $SSLCert' > appsettings.json.tmp
 cp appsettings.json.tmp appsettings.json
-sudo ./PoliFemoBackend
+screen -S backend -p 0 -X stuff "sudo ./PoliFemoBackend\n"
