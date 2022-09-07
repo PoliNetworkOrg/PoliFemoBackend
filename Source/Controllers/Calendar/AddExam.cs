b7d70ef285c6ff8633ca2b992270cd1809251054
Elisabetta <lolely2029@gmail.com>
2022-09-07 18:49:07 +0200

    aggiunta opzione degli esami per il calendario

---
+++ Source/Controllers/Calendar/AddExam.cs
@@ -0,0 +1,115 @@
+#region
+
+using Microsoft.AspNetCore.Mvc;
+using PoliFemoBackend.Source.Data;
+using PoliFemoBackend.Source.Utils;
+using OfficeOpenXml;
+
+#endregion
+
+namespace PoliFemoBackend.Source.Controllers.Calendar;
+
+[ApiController]
+[ApiVersion("1.0")]
+[ApiExplorerSettings(GroupName = "Calendar")]
+[Route("v{version:apiVersion}/calendar/exam")]
+[Route("/calendar/exam")]
+public class AddExam : ControllerBase
+{
+    /// <summary>
+    ///     Adds exams on Database
+    /// </summary>
+    /// <returns>Nothing</returns>
+    /// <response code="200">Exams Added</response>
+    /// <response code="500">Can't connect to server or Exams not Added</response>
+    [MapToApiVersion("1.0")]
+    [HttpPut]
+    public ObjectResult AddExamDb(List<IFormFile> file){
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        //number of worksheet
+        int n = 0;
+        using var package = new ExcelPackage(file[0].OpenReadStream());
+        n = package.Workbook.Worksheets.Count;
+
+        for (int i = 0; i < n; i++)
+        {
+            var worksheet = package.Workbook.Worksheets[i];
+            //number of rows
+            int rows = worksheet.Dimension.Rows;
+            //number of columns
+            int columns = worksheet.Dimension.Columns;
+            for(int j = 5; j <= rows ; j++)
+            {
+                var query = "INSERT IGNORE INTO Exams VALUES (null, '@cod_mat', \"@insegmaneto\", '@sede', '@semestre', \"@docente\", '@orario' , '@giorno', \"@lista\" );";
+                for(int k = 1; k <= columns; k++)
+                {
+                    var cell = worksheet.Cells[j, k];
+                    if (cell.Value != null)
+                    {
+                        switch(k)
+                        {
+                            case 1:
+                                {
+                                    query = query.Replace("@cod_mat", cell.Value.ToString());
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    query = query.Replace("@insegmaneto", cell.Value.ToString());
+                                    break;
+                                }
+                            case 4:
+                                {
+                                    query = query.Replace("@sede", cell.Value.ToString());
+                                    break;
+                                }
+                            case 5:
+                                {
+                                    query = query.Replace("@semestre", cell.Value.ToString());
+                                    break;
+                                }
+                            case 6:
+                                {
+                                    query = query.Replace("@docente", cell.Value.ToString());
+                                    break;
+                                }
+                            case 8:
+                                {
+                                    //verify if date in null or not
+                                    string? c = cell.Value.ToString();
+                                    if (c!=null)
+                                    {
+                                        var date = DateTime.Parse(c);
+                                        //split datetime to time and date
+                                        query = query.Replace("@orario", date.TimeOfDay.ToString("hh\\:mm\\:ss"));
+                                        query = query.Replace("@giorno", date.Date.ToString("yyyy-MM-dd"));
+                                    }
+                                    else
+                                    {
+                                        query = query.Replace("@orario", "null");
+                                        query = query.Replace("@giorno", "null");
+                                    }
+                                    break;
+                                }
+                            case 9:
+                                {
+                                    query = query.Replace("@lista", cell.Value.ToString());
+                                    break;
+                                }
+                            
+                        }
+                        
+                    }
+                }
+                Console.WriteLine(query);
+                var results = Database.Execute(query, GlobalVariables.DbConfigVar);
+            }
+            
+        }
+
+        return Ok("Ok");
+    }
+    
+        
+
+}
