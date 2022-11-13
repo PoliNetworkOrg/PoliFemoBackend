b1b14034e835b3918f32c3051533c358d12bc543
Ely <lolely2029@gmail.com>
2022-11-13 19:59:46 +0100

    endpoint dei tag

---
+++ Source/Controllers/Tags/SearchTags.cs
@@ -0,0 +1,62 @@
+#region
+
+using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using PoliFemoBackend.Source.Data;
+using PoliFemoBackend.Source.Utils;
+
+#endregion
+
+namespace PoliFemoBackend.Source.Controllers.Tags;
+
+[ApiController]
+[ApiVersion("1.0")]
+[ApiExplorerSettings(GroupName = "Tags")]
+[Route("v{version:apiVersion}/tags")]
+[Route("/tags")]
+
+public class TagByIdController : ControllerBase
+{
+    /// <summary>
+    ///     Search Tags
+    /// </summary>
+    /// <returns>A json of tags</returns>
+    /// <response code="200">Returns the array of tags</response>
+    /// <response code="500">Can't connect to server</response>
+    /// <response code="404">No available tags</response>
+
+    [MapToApiVersion("1.0")]
+    [HttpGet]
+    public ActionResult SearchTags()
+    {
+        
+        var results = Database.ExecuteSelect(
+            "SELECT * FROM Tags",
+            GlobalVariables.DbConfigVar);
+
+
+        //if results is null
+        if (results == null) return StatusCode(500);  
+
+        if (results.Rows.Count == 0) return NotFound();
+
+        //convert results to json
+        var a = new JObject();
+        var b = new JArray();
+        foreach(DataRow row in results.Rows)
+        {
+            var c = new JObject();
+            foreach(DataColumn column in results.Columns)
+            {
+                c.Add(column.ColumnName, row[column].ToString());
+            }
+            b.Add(c);
+        }
+        a.Add("tags", b);
+        
+        return Ok(a);
+        
+    }
+}
