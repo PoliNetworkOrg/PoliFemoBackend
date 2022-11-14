39a4ebc08aed4739201ff75df19d392630267d22
user@example.com <user@example.com>
2022-11-14 13:46:26 +0100

    Update ThreadWithAction.cs

--- Source/Objects/Threading/ThreadWithAction.cs
+++ Source/Objects/Threading/ThreadWithAction.cs
@@ -2,5 +2,22 @@
 
 public class ThreadWithAction
 {
-    
+    private readonly Thread _thread;
+    private readonly Action _action;
+
+    public ThreadWithAction(Action action)
+    {
+        this._action = action;
+        this._thread = new Thread(RunThread);
+    }
+
+    public void Run()
+    {
+        this._thread.Start();
+    }
+
+    private void RunThread()
+    {
+        this._action.Invoke();
+    }
 }
