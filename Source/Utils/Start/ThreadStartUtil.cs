436ce17730a2c7c08d57f17935a88af7a52d6c72
user@example.com <user@example.com>
2022-11-14 13:49:49 +0100

    Update ThreadStartUtil.cs

--- Source/Utils/Start/ThreadStartUtil.cs
+++ Source/Utils/Start/ThreadStartUtil.cs
@@ -1,9 +1,14 @@
-﻿namespace PoliFemoBackend.Source.Utils.Start;
+﻿using PoliFemoBackend.Source.Objects.Threading;
+
+namespace PoliFemoBackend.Source.Utils.Start;
 
 public static class ThreadStartUtil
 {
+    private static ThreadWithAction? _getNewsThreadWithAction;
+    
     public static void ThreadStartMethod()
     {
-        
+        _getNewsThreadWithAction = new ThreadWithAction(Utils.News.PoliMiNewsUtil.LoopGetNews);
+        _getNewsThreadWithAction.Run();
     }
 }
