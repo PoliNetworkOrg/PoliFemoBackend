938e95cc5c0008ffe80009b79d9da65b09c73c65
steven9463912 <96684523+steven9463912@users.noreply.github.com>
2022-11-20 11:47:52 +0100

    complete #41 (#49)

--- Source/Controllers/Articles/ById.cs
+++ Source/Controllers/Articles/ById.cs
@@ -1,8 +1,7 @@
 ﻿#region
 
-using System.Data;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
+
 using Newtonsoft.Json.Linq;
 using PoliFemoBackend.Source.Data;
 using PoliFemoBackend.Source.Utils.Database;
@@ -31,6 +30,13 @@ public class ArticleByIdController : ControllerBase
     public ActionResult SearchArticlesById(int id)
     {
         Console.WriteLine(id);
+        var a = SearchArticlesByIdObject(id);
+        return a == null ? NotFound() : Ok(a);
+    }
+
+    public static JObject? SearchArticlesByIdObject(int id)
+    {
+
         var results = Database.ExecuteSelect(
             "SELECT * FROM Articles, Authors  WHERE id_article = @id AND Articles.id_author = Authors.id_author",
             GlobalVariables.DbConfigVar,
@@ -38,40 +44,39 @@ public class ArticleByIdController : ControllerBase
             {
                 { "@id", id }
             });
-
+        
 
 
         //if results is null
-        if (results == null) return NotFound();
+
+        var row = results?.Rows[0];
+        if (row == null)
+            return null;
 
         //convert results to json
-        
         var a = new JObject
         {
-            { "title", results.Rows[0]["title"].ToString() },
-            { "subtitle", results.Rows[0]["subtitle"].ToString()== "" ? null : results.Rows[0]["subtitle"].ToString()  },
-            { "latitude", results.Rows[0]["latitude"].ToString()== "" ? null : Double.Parse(results.Rows[0]["latitude"].ToString() ?? "")  },
-            { "longitude", results.Rows[0]["longitude"].ToString()== "" ? null : Double.Parse(results.Rows[0]["longituide"].ToString() ?? "")  },
+            { "title", row["title"].ToString() },
+            { "subtitle", row["subtitle"].ToString()== "" ? null : row["subtitle"].ToString()  },
+            { "latitude", row["latitude"].ToString()== "" ? null : double.Parse(row["latitude"].ToString() ?? "")  },
+            { "longitude",row["longitude"].ToString()== "" ? null : double.Parse(row["longituide"].ToString() ?? "")  },
             //change format of date
-            { "publish_time", DateTime.Parse(results.Rows[0]["publishTime"].ToString()?? "").ToString("yyyy-MM-dd hh:mm:ss") },
-            { "target_time", DateTime.Parse(results.Rows[0]["targetTime"].ToString()?? "").ToString("yyyy-MM-dd hh:mm:ss") },	
-            { "content", results.Rows[0]["content"].ToString() },
-            { "image", results.Rows[0]["image"].ToString() == "" ? null : results.Rows[0]["image"].ToString()},
+            { "publish_time", Utils.DateTimeUtil.ConvertToDateTime(row["publishTime"].ToString()?? "")?.ToString("yyyy-MM-dd hh:mm:ss") },
+            { "target_time", Utils.DateTimeUtil.ConvertToDateTime(row["targetTime"].ToString()?? "")?.ToString("yyyy-MM-dd hh:mm:ss") },	
+            { "content", row["content"].ToString() },
+            { "image", row["image"].ToString() == "" ? null : row["image"].ToString()},
         };
-
-        if(results.Rows[0]["targetTime"].ToString() == "")
-            a.Add("target_time", null);
+      
         
         var b = new JObject
         {
-            { "name", results.Rows[0]["name_"].ToString() },
-            { "link", results.Rows[0]["link"].ToString() }, 
-            { "image", results.Rows[0]["image1"].ToString()},
+            { "name", row["name_"].ToString() },
+            { "link", row["link"].ToString() }, 
+            { "image", row["image1"].ToString()},
         };
 
         a.Add("author", b);
-        
-        return Ok(a);
-        
+
+        return a;
     }
 }
