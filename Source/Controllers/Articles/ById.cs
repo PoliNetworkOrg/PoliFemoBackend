a0b336a3c0aa1294b2b33b7838eb06ce72c5c862
user@example.com <user@example.com>
2022-11-20 12:08:33 +0100

    Update ById.cs

--- Source/Controllers/Articles/ById.cs
+++ Source/Controllers/Articles/ById.cs
@@ -1,10 +1,7 @@
 ﻿#region
 
 using Microsoft.AspNetCore.Mvc;
-<<<<<<< HEAD
-=======
 
->>>>>>> dev
 using Newtonsoft.Json.Linq;
 using PoliFemoBackend.Source.Data;
 using PoliFemoBackend.Source.Utils.Database;
@@ -21,24 +18,12 @@ namespace PoliFemoBackend.Source.Controllers.Articles;
 public class ArticleByIdController : ControllerBase
 {
     /// <summary>
-<<<<<<< HEAD
     ///     Search article by id
-=======
-    ///    Search article by id
->>>>>>> dev
     /// </summary>
     /// <returns>A json of article</returns>
     /// <response code="200">Returns article</response>
     /// <response code="500">Can't connect to server</response>
     /// <response code="404">No available article</response>
-<<<<<<< HEAD
-    [MapToApiVersion("1.0")]
-    [HttpGet]
-    public ActionResult SearchArticlesById(int id)
-    {
-        Console.WriteLine(id);
-=======
-
     [MapToApiVersion("1.0")]
     [HttpGet]
     public ActionResult SearchArticlesById(int id)
@@ -51,7 +36,7 @@ public class ArticleByIdController : ControllerBase
     public static JObject? SearchArticlesByIdObject(int id)
     {
 
->>>>>>> dev
+
         var results = Database.ExecuteSelect(
             "SELECT * FROM Articles, Authors  WHERE id_article = @id AND Articles.id_author = Authors.id_author",
             GlobalVariables.DbConfigVar,
@@ -92,34 +77,7 @@ public class ArticleByIdController : ControllerBase
 
         a.Add("author", b);
 
-<<<<<<< HEAD
-
-        //if results is null
-        if (results == null) return StatusCode(500);
-
-        if (results.Rows.Count == 0) return NotFound();
-
-        //convert results to json
-        var a = new JObject
-        {
-            { "title", results.Rows[0]["title"].ToString() },
-            { "subtitle", results.Rows[0]["subtitle"].ToString() },
-            { "publishTime", results.Rows[0]["publishTime"].ToString() },
-            { "targetTime", results.Rows[0]["targetTime"].ToString() },
-            { "content", results.Rows[0]["content"].ToString() }
-        };
-        var b = new JObject
-        {
-            { "name", results.Rows[0]["name_"].ToString() },
-            { "image", results.Rows[0]["image"].ToString() },
-            { "link", results.Rows[0]["link"].ToString() }
-        };
-
-        a.Add("author", b);
-
-        return Ok(a);
-=======
         return a;
->>>>>>> dev
+
     }
 }
