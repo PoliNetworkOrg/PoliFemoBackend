9d2ea79fdba6788ad7f717c0b8bcf2a8d58f2a5a
user@example.com <user@example.com>
2022-11-20 13:40:38 +0100

    Update Optional.cs

---
+++ Source/Objects/Types/Optional.cs
@@ -0,0 +1,23 @@
+﻿namespace PoliFemoBackend.Source.Objects.Types;
+
+public class Optional<T> where T : new()
+{
+    private readonly T? _value;
+    public readonly bool IsPresent;
+
+    public Optional(T value)
+    {
+        _value = value;
+        IsPresent = true;
+    }
+
+    public Optional()
+    {
+        IsPresent = false;
+    }
+
+    public T GetValue()
+    {
+        return _value ?? new T();
+    }
+}
