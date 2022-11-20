9c2d3272487833bcdc27e30fd20760c0ad73f264
user@example.com <user@example.com>
2022-11-20 11:38:30 +0100

    Update ById.cs

--- Source/Controllers/Articles/ById.cs
+++ Source/Controllers/Articles/ById.cs
@@ -1,13 +1,8 @@
 ﻿#region
 
-using System.Data;
 using Microsoft.AspNetCore.Mvc;
-<<<<<<<
+
 using Newtonsoft.Json.Linq;
-=======
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
->>>>>>>
 using PoliFemoBackend.Source.Data;
 using PoliFemoBackend.Source.Utils.Database;
 
@@ -34,7 +29,6 @@ public class ArticleByIdController : ControllerBase
     [HttpGet]
     public ActionResult SearchArticlesById(int id)
     {
-<<<<<<<
         Console.WriteLine(id);
         var a = SearchArticlesByIdObject(id);
         return a == null ? NotFound() : Ok(a);
@@ -42,9 +36,7 @@ public class ArticleByIdController : ControllerBase
 
     public static JObject? SearchArticlesByIdObject(int id)
     {
-=======
-        Console.WriteLine(id);
->>>>>>>
+
         var results = Database.ExecuteSelect(
             "SELECT * FROM Articles, Authors  WHERE id_article = @id AND Articles.id_author = Authors.id_author",
             GlobalVariables.DbConfigVar,
@@ -54,7 +46,7 @@ public class ArticleByIdController : ControllerBase
             });
         
 
-<<<<<<<
+
         //if results is null
 
         var row = results?.Rows[0];
@@ -69,8 +61,8 @@ public class ArticleByIdController : ControllerBase
             { "latitude", row["latitude"].ToString()== "" ? null : double.Parse(row["latitude"].ToString() ?? "")  },
             { "longitude",row["longitude"].ToString()== "" ? null : double.Parse(row["longituide"].ToString() ?? "")  },
             //change format of date
-            { "publish_time", DateTimeUtil.ConvertToDateTime(row["publishTime"].ToString()?? "")?.ToString("yyyy-MM-dd hh:mm:ss") },
-            { "target_time", DateTimeUtil.ConvertToDateTime(row["targetTime"].ToString()?? "")?.ToString("yyyy-MM-dd hh:mm:ss") },	
+            { "publish_time", Utils.DateTimeUtil.ConvertToDateTime(row["publishTime"].ToString()?? "")?.ToString("yyyy-MM-dd hh:mm:ss") },
+            { "target_time", Utils.DateTimeUtil.ConvertToDateTime(row["targetTime"].ToString()?? "")?.ToString("yyyy-MM-dd hh:mm:ss") },	
             { "content", row["content"].ToString() },
             { "image", row["image"].ToString() == "" ? null : row["image"].ToString()},
         };
@@ -86,41 +78,5 @@ public class ArticleByIdController : ControllerBase
         a.Add("author", b);
 
         return a;
-=======
-
-
-        //if results is null
-        if (results == null) return NotFound();
-
-        //convert results to json
-        
-        var a = new JObject
-        {
-            { "title", results.Rows[0]["title"].ToString() },
-            { "subtitle", results.Rows[0]["subtitle"].ToString()== "" ? null : results.Rows[0]["subtitle"].ToString()  },
-            { "latitude", results.Rows[0]["latitude"].ToString()== "" ? null : Double.Parse(results.Rows[0]["latitude"].ToString() ?? "")  },
-            { "longitude", results.Rows[0]["longitude"].ToString()== "" ? null : Double.Parse(results.Rows[0]["longituide"].ToString() ?? "")  },
-            //change format of date
-            { "publish_time", DateTime.Parse(results.Rows[0]["publishTime"].ToString()?? "").ToString("yyyy-MM-dd hh:mm:ss") },
-            { "target_time", DateTime.Parse(results.Rows[0]["targetTime"].ToString()?? "").ToString("yyyy-MM-dd hh:mm:ss") },	
-            { "content", results.Rows[0]["content"].ToString() },
-            { "image", results.Rows[0]["image"].ToString() == "" ? null : results.Rows[0]["image"].ToString()},
-        };
-
-        if(results.Rows[0]["targetTime"].ToString() == "")
-            a.Add("target_time", null);
-        
-        var b = new JObject
-        {
-            { "name", results.Rows[0]["name_"].ToString() },
-            { "link", results.Rows[0]["link"].ToString() }, 
-            { "image", results.Rows[0]["image1"].ToString()},
-        };
-
-        a.Add("author", b);
-        
-        return Ok(a);
-        
->>>>>>>
     }
 }
