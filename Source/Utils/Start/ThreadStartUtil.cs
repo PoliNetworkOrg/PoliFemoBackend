66ab43643becd3b9f39b257d5a541833fb68edac
user@example.com <user@example.com>
2022-11-26 00:01:18 +0100

    Update ThreadStartUtil.cs

--- Source/Utils/Start/ThreadStartUtil.cs
+++ Source/Utils/Start/ThreadStartUtil.cs
@@ -1,5 +1,4 @@
 ﻿using PoliFemoBackend.Source.Objects.Threading;
-using PoliFemoBackend.Source.Utils.News;
 using PoliFemoBackend.Source.Utils.News.PoliMi;
 
 namespace PoliFemoBackend.Source.Utils.Start;
