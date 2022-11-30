a8ac2c021d3ee5bc0ae9bf338c0693787f5b02bb
Francesco Lo Faro <francesco@lofa.ml>
2022-11-30 19:39:11 +0100

    Update DeleteArticle.cs

--- Source/Controllers/Articles/DeleteArticle.cs
+++ Source/Controllers/Articles/DeleteArticle.cs
@@ -2,7 +2,6 @@
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using PoliFemoBackend.Source.Data;
 using PoliFemoBackend.Source.Utils;
@@ -17,44 +16,50 @@ namespace PoliFemoBackend.Source.Controllers.Articles;
 [ApiController]
 [ApiVersion("1.0")]
 [ApiExplorerSettings(GroupName = "Articles")]
-[Route("v{version:apiVersion}/[controller]")]
-[Route("[controller]")]
+[Route("v{version:apiVersion}/articles/{id}")]
+[Route("/articles/{id}")]
 public class DeleteArticle : ControllerBase
 {
     /// <summary>
-    ///     Removes article from database
+    ///     Removes an article from database
     /// </summary>
-    /// <param name="id_article">id of the article's to delete</param>
+    /// <param name="id">ID of the article to be deleted</param>
     /// <response code="200">Article deleted successfully</response>
     /// <response code="403">The user does not have enough permissions</response>
     /// <response code="500">Can't connect to server</response>
     [MapToApiVersion("1.0")]
-    [HttpPost]
-    [HttpGet]
+    [HttpDelete]
     [Authorize]
-    public ObjectResult DeleteArticleDb(int id_article)
+    public ObjectResult DeleteArticleDb(int id)
     {
         var sub = AuthUtil.GetSubjectFromHttpRequest(Request);
-        var article = Database.ExecuteSelect($"SELECT id_author from Articles WHERE id_article={id_article}",GlobalVariables.DbConfigVar);
+        var article = Database.ExecuteSelect($"SELECT id_author from Articles WHERE id_article=@id",
+        GlobalVariables.DbConfigVar,
+        new Dictionary<string, object?>
+        {
+            {"@id", id}
+        });
         if(article == null)
-            return new BadRequestObjectResult(new JObject
-            {
-                { "error","no articles found with specified id" }
-            });
+            return new NotFoundObjectResult("");
         var id_author = Convert.ToInt32(Database.GetFirstValueFromDataTable(article));
-        if(!AuthUtil.HasGrantAndObjectPermission(sub, "autori", id_author)){
+        if(!AuthUtil.HasGrantAndObjectPermission(sub, "authors", id_author)){
             Response.StatusCode = 403;
-            return new BadRequestObjectResult(new JObject
+            return new UnauthorizedObjectResult(new JObject
             {
-                { "error","you don't have enough permissions" }
+                { "error","You don't have enough permissions" }
             });
         }
-        var result = Database.Execute($"DELETE FROM Articles WHERE id_article={id_article}",GlobalVariables.DbConfigVar);
+        var result = Database.Execute($"DELETE FROM Articles WHERE id_article=@id",
+        GlobalVariables.DbConfigVar,
+        new Dictionary<string, object?>
+        {
+            {"@id", id}
+        });
         if(result == -1){
             Response.StatusCode = 500;
-            return new BadRequestObjectResult(new JObject
+            return new ObjectResult(new JObject
             {
-                { "error","internal server error" }
+                { "error","Internal server error" }
             });
         }
         return Ok("");
