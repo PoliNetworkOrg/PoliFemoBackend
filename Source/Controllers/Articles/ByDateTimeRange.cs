2bbe9313c3447239befa73c628d5d274c1dd06c5
Francesco Lo Faro <francesco@lofa.ml>
2022-11-30 12:44:43 +0100

    new search format

--- Source/Controllers/Articles/ByDateTimeRange.cs
+++ Source/Controllers/Articles/ByDateTimeRange.cs
@@ -13,39 +13,66 @@ namespace PoliFemoBackend.Source.Controllers.Articles;
 [ApiController]
 [ApiVersion("1.0")]
 [ApiExplorerSettings(GroupName = "Articles")]
-[Route("v{version:apiVersion}/articles/timerange/{start}/{end}")]
-[Route("/articles/timerange/{start}/{end}")]
+[Route("v{version:apiVersion}/articles")]
+[Route("/articles")]
 public class ArticlesByDateTimeRange : ControllerBase
 {
     /// <summary>
     ///     Search articles by time range.
     /// </summary>
+    /// <param name="start">Start time</param>
+    /// <param name="end">End time</param>
+    /// <param name="tag">Tag name</param>
+    /// <param name="limit">Limit of results</param>
     /// <remarks>
-    ///     If one of the parameters is invalid, today's date will be used.
+    ///     At least one of the parameters must be specified.
     /// </remarks>
     /// <returns>A json list of articles</returns>
-    /// <response code="200">Returns articles</response>
+    /// <response code="200">Articles found</response>
     /// <response code="500">Can't connect to server</response>
     /// <response code="404">No available articles</response>
     [MapToApiVersion("1.0")]
     [HttpGet]
-    public ActionResult SearchArticlesByDateRange(string? start, string? end)
+    public ObjectResult SearchArticlesByDateRange(string? start, string? end, string? tag, int? limit)
     {
-        var r = SearchArticlesByDateRangeAsJobject(start, end);
-        return r == null ? NotFound() : Ok(r);
+        if (start == null && end == null && tag == null)
+        {
+            return new BadRequestObjectResult(new
+            {
+                error = "Invalid parameters"
+            });
+        }
+
+        var r = SearchArticlesByParamsAsJobject(start, end, tag, limit);
+        return r == null ? new NotFoundObjectResult("") : Ok(r);
     }
 
-    private static JObject? SearchArticlesByDateRangeAsJobject(string? start, string? end)
+    private static JObject? SearchArticlesByParamsAsJobject(string? start, string? end, string? tag, int? limit)
     {
-        var startDateTime = DateTimeUtil.ConvertToMySqlString(DateTimeUtil.ConvertToDateTime(start) ?? DateTime.Now);
-        var endDateTime = DateTimeUtil.ConvertToMySqlString(DateTimeUtil.ConvertToDateTime(end) ?? DateTime.Now);
+        var startDateTime = DateTimeUtil.ConvertToMySqlString(DateTimeUtil.ConvertToDateTime(start) ?? null);
+        var endDateTime = DateTimeUtil.ConvertToMySqlString(DateTimeUtil.ConvertToDateTime(end) ?? null);
+        var query = "SELECT * FROM ArticlesWithAuthors_View WHERE "; //rifare la view
+        if (start != null) {
+            query += "publishTime >= @start AND ";
+        }
+        if (end != null) {
+            query += "publishTime <= @end AND ";
+        }
+        if (tag != null) {
+            query += "id_tag = @tag AND ";
+        }
+
+        query = query.Substring(0, query.Length - 4);
+        query += " LIMIT " + (limit ?? 30);
+
         var results = Database.ExecuteSelect(
-            "SELECT * FROM ArticlesWithAuthors_View WHERE publishTime >= '@start' AND publishTime <= '@end'",
+            query.Substring(0, query.Length - 4),  // Remove last AND
             GlobalVariables.DbConfigVar,
             new Dictionary<string, object?>
             {
                 { "@start", startDateTime },
-                { "@end", endDateTime }
+                { "@end", endDateTime },
+                { "@tag", tag }
             });
 
         if (results == null || results.Rows.Count == 0)
@@ -57,7 +84,8 @@ public class ArticlesByDateTimeRange : ControllerBase
         {
             ["results"] = resultsJArray,
             ["start"] = startDateTime,
-            ["end"] = endDateTime
+            ["end"] = endDateTime,
+            ["tag"] = tag
         };
         return r;
     }
