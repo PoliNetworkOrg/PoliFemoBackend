431337bbb04b8b1423956c94e91b2ab07c66d21d
Francesco Bazzano <35663791+frabazz@users.noreply.github.com>
2022-12-08 23:14:52 +0100

    added Permissions group and GetPermission endpoint (#65)

---
+++ Source/Controllers/Profile/GetPermission.cs
@@ -0,0 +1,67 @@
+#region
+
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using PoliFemoBackend.Source.Utils;
+using Microsoft.AspNetCore.Authorization;
+// ReSharper disable InconsistentNaming
+
+#endregion
+
+namespace PoliFemoBackend.Source.Controllers.Profile;
+
+[ApiController]
+[ApiVersion("1.0")]
+[ApiExplorerSettings(GroupName = "Account")]
+[Route("v{version:apiVersion}/accounts/{id}/permissions")]
+[Route("accounts/{id}/permissions")]
+[Authorize]
+public class GetPermissions : ControllerBase
+{
+    /// <summary>
+    ///     Returns the permissions of the user
+    /// </summary>
+    /// <param name="id">id of the user</param>
+    /// <response code="200">Permissions returned successfully</response>
+    /// <response code="500">Can't connect to server</response>
+    [MapToApiVersion("1.0")]
+    [HttpGet]
+    public ObjectResult GetPermission(string id)
+    {
+        var perms = AuthUtil.GetPermissions(id, false);
+        if(perms == null){
+            Response.StatusCode = 500;
+            return new BadRequestObjectResult(new JObject
+            {
+                { "error", "Internal server error" }
+            });
+        }
+
+        if (perms.Count == 0)
+        {
+            Response.StatusCode = 404;
+            return new NotFoundObjectResult(new JObject
+            {
+                { "error", "No permissions found" }
+            });
+        }
+
+        var formattedPerms = new List<JObject>();
+        foreach(var t in perms){
+            formattedPerms.Add(new JObject{
+                {"grant", t.name_grant},
+                {"object_id", t.id_object}
+            });
+        }
+
+        return Ok(
+            
+                new JObject
+                {
+                    { "permissions", JToken.FromObject(formattedPerms) }
+                }
+            
+        );
+
+    }
+}
