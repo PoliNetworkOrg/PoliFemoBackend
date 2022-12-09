8182f3cc803d2de1087fa0d12909a34e4c25b0c4
user@example.com <user@example.com>
2022-12-09 19:11:18 +0100

    correct path

--- Source/Controllers/Accounts/PermissionController.cs
+++ Source/Controllers/Accounts/PermissionController.cs
@@ -5,17 +5,50 @@ using PoliFemoBackend.Source.Data;
 using PoliFemoBackend.Source.Utils;
 using PoliFemoBackend.Source.Utils.Database;
 
-namespace PoliFemoBackend.Source.Controllers.Permissions;
+namespace PoliFemoBackend.Source.Controllers.Accounts;
 
 [ApiController]
 [ApiVersion("1.0")]
 [ApiExplorerSettings(GroupName = "Permissions")]
-[Route("v{version:apiVersion}/permissions/revoke")]
-[Route("/permissions/revoke")]
-public class RevokePermissionController : ControllerBase
+[Route("v{version:apiVersion}/permissions/")]
+[Route("/permissions/")]
+public class GrantPermissionController : ControllerBase
 {
     [MapToApiVersion("1.0")]
-    [HttpPost]
+    [HttpPut]
+    [Authorize]
+    public ObjectResult GrantPermission(string idGrant, string idUser, long idObject)
+    {
+        var canGrantPermissions = AuthUtil.HasPermission(AuthUtil.GetSubjectFromHttpRequest(Request),
+            Constants.Permissions.PermissionsConst);
+        if (!canGrantPermissions)
+        {
+            Response.StatusCode = 403;
+            return new ObjectResult(new JObject
+            {
+                { "error", "You don't have enough permissions" }
+            });
+        }
+        
+        const string q = "INSERT INTO permission (id_grant, id_user, id_object) VALUES (@id_grant, @id_user, @id_object)";
+        var count = Database.Execute(q, DbConfig.DbConfigVar, new Dictionary<string, object?>()
+        {
+            {"@id_grant",idGrant},
+            {"@id_user",idUser},
+            {"@id_object",idObject},
+        });
+        
+        if (count > 0)
+            return Ok("");
+        
+        return new BadRequestObjectResult(new JObject
+        {
+            { "error", "Grant failed" }
+        });
+    }
+    
+    [MapToApiVersion("1.0")]
+    [HttpDelete]
     [Authorize]
     public ObjectResult RevokePermission(string idGrant, string idUser, long idObject)
     {
