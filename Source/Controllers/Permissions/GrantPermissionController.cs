b4105e136f8f63efc86a2a39fc16ae8e87845326
user@example.com <user@example.com>
2022-12-09 18:46:53 +0100

    grant permission

---
+++ Source/Controllers/Permissions/GrantPermissionController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using PoliFemoBackend.Source.Utils.Database;
+
+namespace PoliFemoBackend.Source.Controllers.Permissions;
+
+[ApiController]
+[ApiVersion("1.0")]
+[ApiExplorerSettings(GroupName = "Permissions")]
+[Route("v{version:apiVersion}/permissions/grant")]
+[Route("/permissions/grant")]
+public class GrantPermissionController : ControllerBase
+{
+    [MapToApiVersion("1.0")]
+    [HttpPost]
+    [Authorize]
+    public ObjectResult GrantPermission(string idGrant, string idUser, long idObject)
+    {
+        // id_grant, id_user, id_object
+        const string q = "INSERT INTO permission (id_grant, id_user, id_object) VALUES (@id_grant, @id_user, @id_object)";
+        var count = Database.Execute(q, DbConfig.DbConfigVar, new Dictionary<string, object?>()
+        {
+            {"@id_grant",idGrant},
+            {"@id_user",idUser},
+            {"@id_object",idObject},
+        });
+        
+        if (count > 0)
+            return Ok("");
+        
+        return new BadRequestObjectResult(new JObject
+        {
+            { "error", "Grant failed" }
+        });
+    }
+}
