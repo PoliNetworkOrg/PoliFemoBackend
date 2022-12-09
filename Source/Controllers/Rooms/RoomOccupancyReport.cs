f16dad6ee84d50e1f716fd783334454db08fe8e8
user@example.com <user@example.com>
2022-12-09 21:40:51 +0100

    get rate

--- Source/Controllers/Rooms/RoomOccupancyReport.cs
+++ Source/Controllers/Rooms/RoomOccupancyReport.cs
@@ -20,7 +20,7 @@ public class RoomOccupancyReport : ControllerBase
     public ObjectResult ReportOccupancy(string room, float rate)
     {
         var whenReported = DateTime.Now;
-        
+
         var user = AuthUtil.GetSubjectFromHttpRequest(this.Request);
         if (string.IsNullOrEmpty(user))
             return new ObjectResult(new JObject
@@ -43,7 +43,7 @@ public class RoomOccupancyReport : ControllerBase
                 { "error", "Rate must between " + Constants.MinRate + " and " + Constants.MaxRate }
             });
         }
-        
+
         //check dominio = polimi
         var domain = AuthUtil.GetDomainFromHttpRequest(this.Request);
         if (domain != "polimi.it")
@@ -52,13 +52,14 @@ public class RoomOccupancyReport : ControllerBase
                 { "error", "You don't have enough permissions" }
             });
 
-        var q = "INSERT INTO RoomOccupancyReport (id_room, id_user, rate, when_reported) VALUES (@id_room, @id_user, @rate, @when_reported)";
+        var q =
+            "REPLACE INTO RoomOccupancyReport (id_room, id_user, rate, when_reported) VALUES (@id_room, @id_user, @rate, @when_reported)";
         var count = Database.Execute(q, DbConfig.DbConfigVar, new Dictionary<string, object?>()
         {
-            {"@id_room", room},
-            {"@id_user", user},
-            {"@rate", rate},
-            {"@when_reported", whenReported}
+            { "@id_room", room },
+            { "@id_user", user },
+            { "@rate", rate },
+            { "@when_reported", whenReported }
         });
 
         if (count <= 0)
@@ -68,16 +69,39 @@ public class RoomOccupancyReport : ControllerBase
                 { "error", "Report failed." }
             });
         }
-        
+
         return Ok("");
     }
-    
+
     [MapToApiVersion("1.0")]
     [HttpGet]
     [Authorize]
     public ObjectResult GetReportedOccupancy(string room)
     {
-        //todo
-        return Ok("");
+        const string q = "SELECT SUM(x.w * x.rate)/SUM(x.w) " +
+                         "FROM (" +
+                         "SELECT TIMESTAMPDIFF(SECOND, NOW(), when_reported) w, rate " +
+                         "FROM RoomOccupancyReport " +
+                         "WHERE room = @room AND when_reported >= @yesterday" +
+                         ") x ";
+        var dict = new Dictionary<string, object?>()
+        {
+            { "@id_room", room },
+            { "@yesterday", DateTime.Now.AddDays(-1) }
+        };
+        var r = Database.ExecuteSelect(q, DbConfig.DbConfigVar, dict);
+        if (r == null || r.Rows.Count == 0 || r.Rows[0].ItemArray.Length == 0)
+            return new ObjectResult(new JObject
+            {
+                { "error", "Can't get occupancy for room " + room }
+            });
+
+        var rate = Database.GetFirstValueFromDataTable(r);
+
+        return Ok(new JObject
+        {
+            { "room", room },
+            { "rate", new JValue(rate) }
+        });
     }
 }
