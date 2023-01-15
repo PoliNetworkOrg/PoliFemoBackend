8695dbdea8f35b9c0ab1df5515aaf98d269ccc14
Eliaxie <elia.maggioni@gmail.com>
2023-01-15 18:28:41 +0100

    adjusted config to work in cloud (#86)
    * changed endpoint
    
    * .
    
    * port 5000
    
    * port 5000
    
    * /
    
    * .
    
    * .
    
    * .
    
    * .
    
    * correcting host
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * clean

---
+++ Source/Controllers/HomePage/HomepageController.cs
@@ -0,0 +1,23 @@
+﻿#region
+
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PoliFemoBackend.Source.Objects.Permission;
+using PoliFemoBackend.Source.Utils;
+
+#endregion
+
+namespace PoliFemoBackend.Source.Controllers.Accounts;
+
+[ApiController]
+[Route("/")]
+public class HomepageByIdController : ControllerBase
+{
+    [HttpGet]
+    public string Index()
+    {
+        return "I'm up";
+    }
+}
