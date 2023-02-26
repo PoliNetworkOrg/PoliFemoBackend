129e333ad886b618ed2ddbc45dc1be842e62b37a
Francesco Lo Faro <francesco@lofa.ml>
2023-02-26 12:49:49 +0100

    Fix permission queries (#123)
    * fix #120
    
    * Fixed permission-related queries
    
    ---------
    
    Co-authored-by: github.nouser@nouser.github.com <github.nouser@nouser.github.com>

--- Source/Controllers/Accounts/EditPermissions.cs
+++ Source/Controllers/Accounts/EditPermissions.cs
@@ -24,7 +24,7 @@ public class EditPermissions : ControllerBase
     /// <response code="200">Request completed successfully</response>
     /// <response code="401">Authorization error</response>
     /// <response code="403">The user does not have enough permissions</response>
-    /// <response code="400">A received grant is not valid</response>
+    /// <response code="400">The permission was already granted</response>
     /// <response code="500">Can't connect to the server</response>
     [HttpPost]
     [Authorize]
@@ -42,19 +42,25 @@ public class EditPermissions : ControllerBase
         }
 
         const string q =
-            "INSERT INTO permissions (grant_id, user_id, object_id) VALUES (@id_grant, @id_user, @id_object)";
+            "INSERT IGNORE INTO permissions (grant_id, user_id, object_id) VALUES (@id_grant, @id_user, @id_object)";
         try
         {
             var count = Database.Execute(q, DbConfig.DbConfigVar, new Dictionary<string, object?>
             {
                 { "@id_grant", grant.grant },
                 { "@id_user", id },
-                { "@id_object", grant.object_id }
+                { "@id_object", grant.object_id ?? -1 }
             });
+
+            if (count != 1)
+                return new BadRequestObjectResult(new JObject
+                {
+                    { "error", "Grant failed. Is the permission already granted?" }
+                });
         }
         catch (Exception)
         {
-            return new BadRequestObjectResult(new JObject
+            return StatusCode(500, new JObject
             {
                 { "error", "An error occurred while granting the permission. Make sure the grant is valid" }
             });
@@ -73,6 +79,7 @@ public class EditPermissions : ControllerBase
     /// <param name="id">User ID</param>
     /// <param name="grant">The Grant object</param>
     /// <response code="200">Permissions updated successfully</response>
+    /// <response code="400">The selected permission was already revoked</response>
     /// <response code="403">The user does not have enough permissions</response>
     [HttpDelete]
     [Authorize]
@@ -95,8 +102,14 @@ public class EditPermissions : ControllerBase
         {
             { "@id_grant", grant.grant },
             { "@id_user", id },
-            { "@id_object", grant.object_id }
+            { "@id_object", grant.object_id ?? -1 }
         });
+        
+        if (count != 1)
+            return new BadRequestObjectResult(new JObject
+            {
+                { "error", "Revoke failed. Is the permission already revoked?" }
+            });
         return Ok("");
     }
 }
