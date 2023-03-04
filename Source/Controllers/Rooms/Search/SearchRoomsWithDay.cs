7ad7874820a8b70c87d31521bc9ec528fcc111ab
Francesco Lo Faro <francesco@lofa.ml>
2023-03-04 12:21:56 +0100

    DB webcache v1 (#128)
    * Added node name
    
    * Removed 'text' field when room is free
    
    * WebCache
    
    * date parameter to a GET-compatible format

--- Source/Controllers/Rooms/Search/SearchRoomsWithDay.cs
+++ Source/Controllers/Rooms/Search/SearchRoomsWithDay.cs
@@ -19,15 +19,21 @@ public class SearchRoomsWithDayController : ControllerBase
     ///     Search for available rooms in a given date, from 8:00 to 20:00
     /// </summary>
     /// <param name="sede" example="MIA">Possible values: MIA, MIB, LCF, MNI, PCL</param>
-    /// <param name="dateOnly">Date</param>
+    /// <param name="date">Date in format yyyy-mm-dd</param>
     /// <returns>A JSON array of free rooms</returns>
     /// <response code="200">Request completed successfully</response>
     /// <response code="204">No available rooms</response>
     /// <response code="500">Can't connect to poli servers</response>
     [HttpGet]
     [ResponseCache(VaryByQueryKeys = new[] { "*" }, Duration = SecondsToCacheSearch)]
-    public async Task<IActionResult> SearchRooms([BindRequired] string sede, [BindRequired] DateOnly dateOnly)
+    public async Task<IActionResult> SearchRooms([BindRequired] string sede, [BindRequired] string date)
     {
+        DateOnly dateOnly;
+        try {
+            dateOnly = DateOnly.Parse(date);
+        } catch (Exception) {
+            return BadRequest(new { error = "Invalid date format" });
+        }
         var hourStart = new DateTime(dateOnly.Year, dateOnly.Month, dateOnly.Day, 8, 0, 0);
         var hourStop = new DateTime(dateOnly.Year, dateOnly.Month, dateOnly.Day, 20, 0, 0);
 
