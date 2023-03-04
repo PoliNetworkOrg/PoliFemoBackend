e3cca2890f3ec3a5558c46d36e6062decff69eaf
Francesco Lo Faro <francesco@lofa.ml>
2023-03-04 11:28:52 +0100

    WebCache

--- Source/Utils/Html/HtmlUtil.cs
+++ Source/Utils/Html/HtmlUtil.cs
@@ -2,6 +2,8 @@
 
 using System.Net;
 using System.Text;
+using HtmlAgilityPack;
+using PoliFemoBackend.Source.Data;
 using PoliFemoBackend.Source.Objects.Web;
 
 #endregion
@@ -10,16 +12,31 @@ namespace PoliFemoBackend.Source.Utils.Html;
 
 public static class HtmlUtil
 {
-    internal static Task<WebReply> DownloadHtmlAsync(string urlAddress)
+    internal static Task<WebReply> DownloadHtmlAsync(string urlAddress, bool isRoomTable = false)
 
     {
         try
         {
+            var q = Database.Database.ExecuteSelect("SELECT * FROM WebCache WHERE url = @url", GlobalVariables.DbConfigVar, new Dictionary<string, object?> {{"@url", urlAddress}});
+            if (q?.Rows.Count > 0)
+            {
+                var sq = q?.Rows[0]["content"]?.ToString();
+                if (sq != null) return Task.FromResult(new WebReply(sq, HttpStatusCode.OK));
+            }
             HttpClient httpClient = new();
             var task = httpClient.GetByteArrayAsync(urlAddress);
             task.Wait();
             var response = task.Result;
             var s = Encoding.UTF8.GetString(response, 0, response.Length);
+            if (isRoomTable) {
+                var doc = new HtmlDocument();
+                doc.LoadHtml(s);
+                var t1 = NodeUtil.GetElementsByTagAndClassName(doc.DocumentNode, "", "BoxInfoCard", 1);
+                var t3 = NodeUtil.GetElementsByTagAndClassName(t1?[0], "", "scrollContent");
+                s = t3?[0].InnerHtml ?? "";
+
+            }
+            Database.Database.Execute("INSERT INTO WebCache (url, content, expires_at) VALUES (@url, @content, NOW())", GlobalVariables.DbConfigVar, new Dictionary<string, object?> {{"@url", urlAddress}, {"@content", s}});
             return Task.FromResult(new WebReply(s, HttpStatusCode.OK));
             /*
 
