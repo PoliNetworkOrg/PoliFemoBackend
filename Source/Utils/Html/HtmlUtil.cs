e9a21da1a4a0298270ba7a604da4bed3893efe3a
Francesco Lo Faro <francesco@lofa.ml>
2023-03-21 21:08:38 +0100

    typo

--- Source/Utils/Html/HtmlUtil.cs
+++ Source/Utils/Html/HtmlUtil.cs
@@ -69,7 +69,7 @@ public static class HtmlUtil
         {
             var dictionary = new Dictionary<string, object?> { { "@url", urlAddress }, { "@content", s } };
             const string q =
-                "INSERT INTO WebCache (url, content, expires_at) VALUES (@url, @content, NOW() + INTERVAL 2 DAYS)";
+                "INSERT INTO WebCache (url, content, expires_at) VALUES (@url, @content, NOW() + INTERVAL 2 DAY)";
             Database.Database.Execute(q, GlobalVariables.DbConfigVar, dictionary);
         }
         catch
