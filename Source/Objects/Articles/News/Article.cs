afbaad73c61a5c93b8c775b935f0ff0d4ca04ec7
Francesco Lo Faro <francesco@lofa.ml>
2023-04-04 23:04:37 +0200

    Platform and hidden until parameters

--- Source/Objects/Articles/News/Article.cs
+++ Source/Objects/Articles/News/Article.cs
@@ -3,7 +3,7 @@ namespace PoliFemoBackend.Source.Objects.Articles.News;
 public class Article
 {
     public Article(string title, string content, int author_id, string tag_id, string? subtitle, string? image,
-        DateTime? target_time, double? latitude, double? longitude, string? blurhash)
+        DateTime? target_time, DateTime? hidden_until, double? latitude, double? longitude, string? blurhash, int platforms)
     {
         this.title = title;
         this.content = content;
@@ -12,9 +12,11 @@ public class Article
         this.subtitle = subtitle;
         this.image = image;
         this.target_time = target_time;
+        this.hidden_until = hidden_until;
         this.latitude = latitude;
         this.longitude = longitude;
         this.blurhash = blurhash;
+        this.platforms = platforms;
     }
 
     public string title { get; set; }
@@ -24,7 +26,9 @@ public class Article
     public string? subtitle { get; set; }
     public string? image { get; set; }
     public DateTime? target_time { get; set; }
+    public DateTime? hidden_until { get; set; }
     public double? latitude { get; set; }
     public double? longitude { get; set; }
     public string? blurhash { get; set; }
+    public int platforms { get; set; }
 }
