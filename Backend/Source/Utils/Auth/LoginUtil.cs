207f1a32f36966918b9516d8e7afacb79962341e
angeousta <132761637+angeousta@users.noreply.github.com>
2023-12-04 20:23:07 +0100

    Update formatting.yml (#247)
    * Update formatting.yml
    
    * CSharpier PR (#248)
    
    Co-authored-by: angeousta <angeousta@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: angeousta <angeousta@users.noreply.github.com>

--- Backend/Source/Utils/Auth/LoginUtil.cs
+++ Backend/Source/Utils/Auth/LoginUtil.cs
@@ -25,9 +25,9 @@ public static class LoginUtil
 
         try
         {
-            token = GlobalVariables.TokenHandler?.ReadJwtToken(
-                responseJson["access_token"]?.ToString()
-            );
+            token = GlobalVariables
+                .TokenHandler
+                ?.ReadJwtToken(responseJson["access_token"]?.ToString());
             var domain = token?.Payload["upn"].ToString()?.Split('@')[1];
             if (domain == null || token?.Subject == null)
             {
@@ -63,16 +63,16 @@ public static class LoginUtil
                 }
             }
 
-            token = GlobalVariables.TokenHandler?.ReadJwtToken(
-                responseJson["id_token"]?.ToString()
-            );
+            token = GlobalVariables
+                .TokenHandler
+                ?.ReadJwtToken(responseJson["id_token"]?.ToString());
             subject = token != null ? token.Subject : throw new Exception("Token is null");
         }
         catch (ArgumentException)
         {
-            token = GlobalVariables.TokenHandler?.ReadJwtToken(
-                responseJson["id_token"]?.ToString()
-            );
+            token = GlobalVariables
+                .TokenHandler
+                ?.ReadJwtToken(responseJson["id_token"]?.ToString());
             subject =
                 token?.Subject
                 ?? throw new Exception(
